7b229f5c076709396115193497e872949064b67a
wangliang <403427912@qq.com>
2015-03-30 14:24:40 +0800

    no message

---
+++ tset.lua
@@ -0,0 +1,26 @@
+-- 
+-- local ToolsUtil={a=3}
+
+-- print(ToolsUtil.a)
+-- function ToolsUtil.xx(xx)
+-- 	print(xx)
+	
+-- end
+-- ToolsUtil:xx(2)
+
+-- require("libcocos2dlua")
+-- $gcc  -o c.o c.c
+
+-- local function b(b1)
+-- 	print(b1)
+-- end
+-- local function a(a1,a2)
+-- 	b=nil
+-- 	a1(a2)
+-- end
+
+-- a(b,"傻逼")
+
+
+local a=require("src.test1")
+a.show()
